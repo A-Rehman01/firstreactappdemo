0750784f1f289cd2521db552a37857bc3cf0c96d
abdulrehman <arehmana377@gmail.com>
2020-06-26 17:58:30 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Bootcamp2020sec Assignment2 Complete
+     First React App  
+    </div>
+  );
+}
+
+export default App;
